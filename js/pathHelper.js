4aaae590d2dd88a6d97ae6ff68f16aa13b99fa57
AntoniosMilad <antoniosmiladibraheim@gmail.com>
2025-03-09 04:02:54 +0200

    fix

--- js/pathHelper.js
+++ js/pathHelper.js
@@ -17,6 +17,16 @@ function updateServiceLinks() {
     "wish list": "en/services/wish-list/index.html",
     "media centre": "en/services/media-centre/index.html",
     about: "en/inspiration/index.html",
+    contact: "en/services/contact/index.html",
+
+    products: "en/products/index.html",
+    "bathroom design": "en/products/bathroom-designs/index.html",
+    "design elements": "en/products/design-elements/index.html",
+    "electric appliances": "en/products/electric-appliances/index.html",
+    "interior fittings": "en/products/interior-fittings/index.html",
+    "kitchens": "en/products/kitchens/index.html",
+    "living": "en/products/living/index.html",
+
   };
 
   // Find all <a> tags and update them if title matches
